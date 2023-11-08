561cfb55415e1eeeaf3fae628f844013610920e7
Amir <amirhamza27940@gmail.com>
2023-11-09 00:56:13 +0600

    Megakit Business Website

---
+++ java_script/app.js
@@ -0,0 +1,53 @@
+$(function() {
+    $('#slider').rotateSlider({
+  
+    });
+   
+  });
+  $(function() {
+    var $button = $.backToTop({
+
+// background color
+backgroundColor: ' hsl(19, 100%, 50%)',
+
+// text color
+color: '#FFFFFF',
+
+// container element
+container: this._body, 
+
+// 'nonn', 'spin', 'fade', 'zoom', or 'spin-inverse'
+effect: 'spin-inverse',
+
+// enable the back to top button
+enabled: true, 
+
+// width/height of the back to top button
+height: 65,  
+width: 65,
+
+// icon
+icon: 'fas fa-chevron-up',
+
+// margins 
+marginX: 20,
+marginY: 20,  
+
+// bottom/top left/right
+position: 'bottom right',           
+
+// trigger position
+pxToTrigger: 600,
+
+// scroll animation
+
+
+// or 'fawesome'
+theme: 'default',
+
+// z-index
+zIndex: 999
+
+});
+
+});
